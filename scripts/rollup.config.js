e2777d1056d8e12f76f6d8fcc00de0d35f354822
Phil Pluckthun <phil@kitten.sh>
2019-09-28 19:16:06 +0100

    Add code-split automatic expansion build
    This adds a code-split build that automatically builds three modules:
    react, react-dom, and prop-types, all as ESM .js files, that expose
    all the CJS exported keys, plus a default export.
    
    The index.js file behaves as before and exports all exports from
    react.js plus ReactDOM and PropTypes.
    
    These are all separately code-split bundles from rollup.
    The dev build is still at `/dev`.

---
+++ scripts/rollup.config.js
@@ -0,0 +1,76 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+
+import expandExportsPlugin from './expand-exports-plugin';
+
+const exportsMap = (isProduction = false) => ({
+  react: `react/cjs/react.${isProduction ? 'production.min' : 'development'}.js`,
+  'react-dom': `react-dom/cjs/react-dom.${isProduction ? 'production.min' : 'development'}.js`,
+  'prop-types': 'prop-types/index.js',
+});
+
+const config = (isProduction = false) => ({
+  input: {
+    index: './src/index.js',
+    react: './src/react.js',
+    'react-dom': './src/react-dom.js',
+    'prop-types': './src/prop-types.js'
+  },
+  plugins: [
+    nodeResolve({
+      mainFields: ['module', 'jsnext', 'main'],
+      browser: true,
+    }),
+    commonjs({
+      ignoreGlobal: true,
+      include: /\/node_modules\//,
+      namedExports: {
+        react: Object.keys(require('react')),
+        'react-dom': Object.keys(require('react-dom')),
+        'prop-types': Object.keys(require('prop-types')),
+      },
+    }),
+    babel({
+      babelrc: false,
+      plugins: [
+        // This expands all our exports
+        [expandExportsPlugin, {
+          map: exportsMap(isProduction)
+        }]
+      ],
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(
+        isProduction ? 'production' : 'development'
+      )
+    })
+  ],
+  onwarn: () => {},
+  treeshake: {
+    moduleSideEffects: false,
+    propertyReadSideEffects: false
+  }
+});
+
+export default [
+  {
+    ...config(true),
+    output: {
+      dir: './',
+      entryFileNames: '[name].js',
+      chunkFileNames: '[name]-[hash].js',
+      format: 'esm'
+    }
+  },
+  {
+    ...config(false),
+    output: {
+      dir: './dev',
+      entryFileNames: '[name].js',
+      chunkFileNames: '[name]-[hash].js',
+      format: 'esm'
+    }
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from './react';
+import ReactDOM from './react-dom';
+import PropTypes from './prop-types';
+
+export { React, ReactDOM, PropTypes };
+export * from './react';

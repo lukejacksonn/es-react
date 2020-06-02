119f73b4b103aab4ca8a0550c82d3db3f34b87de
Nate Jacobs <iAmNathanJ@users.noreply.github.com>
2020-06-02 06:52:23 -0400

    feat: add entry point for react-dom-server-browser (#13)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from './react';
 import ReactDOM from './react-dom';
+import ReactDOMServerBrowser from './react-dom';
 import PropTypes from './prop-types';
 
-export { React, ReactDOM, PropTypes };
+export { React, ReactDOM, ReactDOMServerBrowser, PropTypes };
 export * from './react';
 export default React;

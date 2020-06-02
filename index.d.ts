a4076e9ee89cca14b06a148906036e0fcffe0ca8
Vladimir Guguiev <wizardzloy@gmail.com>
2020-06-02 13:11:55 +0200

    Add TypeScript definitions (#14)

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import * as React from './react';
+import * as ReactDOM from './react-dom';
+import * as PropTypes from './prop-types';
+
+export { React, ReactDOM, PropTypes };
+export * from './react';
+export default React;
